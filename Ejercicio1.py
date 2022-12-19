af5850966243cc20cf68d30861ea70ab4049167f
Manoloserranoo <manuel.serranovallejo@usp.ceu.es>
2022-12-19 09:16:24 +0100

    Update Ejercicio1.py
    cambios ej 1

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,17 +1,25 @@
-def resolver_nonograma(filas,columnas,pistf,pistc)
-    matriz = [[0 for i in range(columnas)] for j in range(filas)]
+def resolver_nonograma(filas,columnas,pistf,pistc):
+    matriz = [[0 for k in range(columnas)] for k in range(filas)]
 
     for i in range(filas):
-        usar_pistas(matriz,j,pistc[j])
+        usar_pistas(matriz,i,pistc[i])
         return matriz
+    for j in range (columnas):
+        usar_pistas(matriz,j,pistf[j])
+        return matriz
+def usar_pistas(matriz,index,pistas,a = True):
+    i = 0
+    for p in pistas:
+        for k in range(p):
+            if a :
+                matriz[index][i] = 1
+            else:
+                matriz[i][index] = 1
+            i = i + 1
+        i = i + 1
+    
 
-def usar_pistas(matriz,fila,pista):
-    for i in range(len(pista)):
-        for j in range(pista[i]):
-            matriz[fila][j] = 1
-        matriz[fila][j] = 2
-    return matriz
-
+    
 
 
 
@@ -21,5 +29,5 @@ if __name__ == "__main__":
     filas = 5
     columnas = 5
     pistc = [[1],[1,1],[1,1,1],[3],[1]]
-    pistc = [[1],[2],[3],[2,1],[4]]
+    pistf = [[1],[2],[3],[2,1],[4]]
     print(resolver_nonograma(filas,columnas,pistf,pistc))
